27ce29a76ae1a1c8b64a23439e6067f143ab0dc7
Jeffrey Gaitan <86032349+jeffreyg8940@users.noreply.github.com>
2023-11-12 21:01:02 -0500

    Setting up the base stuff for React!

---
+++ watchwish/src/App.tsx
@@ -0,0 +1,10 @@
+import Nav from "./components/navbar";
+function App() {
+  return (
+    <>
+      <Nav></Nav>
+    </>
+  );
+}
+
+export default App;
---
+++ watchwish/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+function Nav() {
+    return(
+        <>
+        <h1>Welcome to the start of our website!!</h1>
+        </>
+    )
+}
+
+export default Nav
